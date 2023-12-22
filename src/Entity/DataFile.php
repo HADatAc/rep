cca42f95bfd9c30791113c45120221c21d225e7b
Paulo Pinheiro <paulopinheiro@porto.local>
2023-12-22 07:54:37 +0000

    addidng further support for datafile

--- src/Entity/DataFile.php
+++ src/Entity/DataFile.php
@@ -3,6 +3,7 @@
 namespace Drupal\rep\Entity;
 
 use Drupal\rep\Vocabulary\REPGUI;
+use Drupal\rep\Constant;
 use Drupal\rep\Utils;
 
 class DataFile {
@@ -15,13 +16,14 @@ class DataFile {
       'element_last_time' => t('Last Process Time'),
       'element_filename' => t('FileName'),
       'element_id' => t('FileId'),
-    ];
-  
+      'element_log' => t('Log'),
+      'element_download' => t('Download'),
+    ];  
   }
 
   public static function generateOutput($list) {
     
-    dpm($list);
+    //dpm($list);
 
     // ROOT URL
     $root_url = \Drupal::request()->getBaseUrl();
@@ -32,7 +34,6 @@ class DataFile {
     }
 
     foreach ($list as $element) {
-      //dpm($element);
       $uri = ' ';
       if ($element->uri != NULL) {
         $uri = $element->uri;
@@ -45,24 +46,47 @@ class DataFile {
       $id = ' ';
       if ($element->id != NULL && $element->id != '') {
         $id = $element->id;
+        $file_entity = \Drupal\file\Entity\File::load($element->id);
+        if ($file_entity != NULL) {
+          $id .= " (available)";
+        } else {
+          $id .= " (unavailable)";
+        }
       }
-      $status = ' ';
-      if ($element->fileStatus != NULL) {
-        $status - $element->status;
+      $filestatus = ' ';
+      if (isset($element->fileStatus) && $element->fileStatus != NULL) {
+        if ($element->fileStatus == Constant::FILE_STATUS_UNPROCESSED) {
+          $filestatus = '<b><font style="color:#ff0000;">'.Constant::FILE_STATUS_UNPROCESSED.'</font></b>';
+        } else if ($element->fileStatus == Constant::FILE_STATUS_PROCESSED) {
+          $filestatus = '<b><font style="color:#008000;">'.Constant::FILE_STATUS_PROCESSED.'</font></b>';
+        } else if ($element->fileStatus == Constant::FILE_STATUS_WORKING) {
+          $filestatus = '<b><font style="color:#ffA500;">'.Constant::FILE_STATUS_WORKING.'</font></b>';
+        } 
       };
       $lastTime = ' ';
       if ($element->lastProcessTime != NULL) {
-        $lastTime - $element->lastProcessTime;
+        $lastTime = $element->lastProcessTime;
       };
       $root_url = \Drupal::request()->getBaseUrl();
+      if (isset($element->log) && $element->log != NULL) {
+        $showLogLink = $root_url.REPGUI::DATAFILE_LOG.base64_encode($element->uri);
+        $log = '<a href="' . $showLogLink . '" class="use-ajax btn btn-primary btn-sm" '.
+               'data-dialog-type="modal" '.
+               'data-dialog-options=\'{"width": 700}\' role="button">Read</a>';
+      } else {
+        $log = '<a href="#link" class="btn btn-primary btn-sm" role="button">Read</a>';
+      }
+      $downloadLink = $root_url.REPGUI::DATAFILE_DOWNLOAD.base64_encode($element->uri);
+      $download = '<a href="'.$downloadLink.'" class="btn btn-primary btn-sm" role="button" disabled>Get It</a>';
       $encodedUri = rawurlencode(rawurlencode($element->uri));
       $output[$element->uri] = [
         'element_uri' => t('<a href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($uri).'">'.$uri.'</a>'),     
-        //'element_status' => t('<b><font style="color:#ff0000;">UNINGESTED</font></b>'),    
-        'element_status' => $status,    
+        'element_status' => t($filestatus),    
         'element_last_time' => $lastTime,    
         'element_filename' => $filename,
         'element_id' => $id,
+        'element_log' => t($log),
+        'element_download' => t($download),
       ];
     }
     return $output;
--- src/Vocabulary/REPGUI.php
+++ src/Vocabulary/REPGUI.php
@@ -7,15 +7,25 @@
     /**
      *   The module name for LIST_PAGE is added inside of ListKeywordLanguagePage 
      */
-    const LIST_PAGE      = "/list/";
+    const LIST_PAGE           = "/list/";
 
     /**
      *   The module name for SELECT_PAGE is added inside of ListManageEmailPage 
      */
-    const SELECT_PAGE    = "/select/";
+    const SELECT_PAGE         = "/select/";
 
-    const DESCRIBE_PAGE  = "/rep/uri/";
+    /**
+     *   Show the log page of a data file 
+     */
+    const DATAFILE_LOG        = "/rep/log/";
 
-    const DOWNLOAD       = "/sir/download/";
+    /**
+     *   Show the log page of a data file 
+     */
+    const DATAFILE_DOWNLOAD   = "/rep/download/";
+
+    const DESCRIBE_PAGE       = "/rep/uri/";
+
+    const DOWNLOAD            = "/sir/download/";
 
   }
