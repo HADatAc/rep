9c31a0fcc495e0363eaadf48fa4da35067397414
Paulo Pinheiro <pp3223@gmail.com>
2025-01-22 15:39:47 +0000

    fixing entrypoints

--- src/EntryPoints.php
+++ src/EntryPoints.php
@@ -4,24 +4,25 @@
 
   use Drupal\rep\Vocabulary\FOAF;
   use Drupal\rep\Vocabulary\HASCO;
+  use Drupal\rep\Vocabulary\PROV;
   use Drupal\rep\Vocabulary\SIO;
   use Drupal\rep\Vocabulary\VSTOI;
 
   class EntryPoints {
 
-    const ANNOTATION_STEM       = VSTOI::VSTOI . "AnnotationStem";
-    const ATTRIBUTE             = SIO::SIO     . "SIO_000614";
-    const DETECTOR_STEM         = VSTOI::VSTOI . "DetectorStem";
-    const ENTITY                = SIO::SIO     . "SIO_000776";
-    const GROUP                 = FOAF::FOAF   . "Group";
-    const INSTRUMENT            = VSTOI::VSTOI . "Instrument";
-    const ORGANIZATION          = FOAF::FOAF   . "Organization";
-    const PERSON                = FOAF::FOAF   . "Person";
-    const PLATFORM              = VSTOI::VSTOI . "Platform";
-    const PROCESS_STEM          = VSTOI::VSTOI . "ProcessStem";
-    const QUESTIONNAIRE         = VSTOI::VSTOI . "Questionnaire";
-    const RESPONSE_OPTION       = VSTOI::VSTOI . "ResponseOption";
-    const STUDY                 = HASCO::HASCO . "Study";
-    const UNIT                  = SIO::SIO     . "SIO_000052";
+    const ANNOTATION_STEM       = VSTOI::ANNOTATION_STEM;
+    const ATTRIBUTE             = SIO::ATTRIBUTE;
+    const DETECTOR_STEM         = VSTOI::DETECTOR_STEM;
+    const ENTITY                = SIO::ENTITY;
+    const GROUP                 = FOAF::GROUP;
+    const INSTRUMENT            = VSTOI::INSTRUMENT;
+    const ORGANIZATION          = FOAF::ORGANIZATION;
+    const PERSON                = FOAF::PERSON;
+    const PLATFORM              = VSTOI::PLATFORM;
+    const PROCESS_STEM          = PROV::ACTIVITY;
+    const QUESTIONNAIRE         = VSTOI::QUESTIONNAIRE;
+    const RESPONSE_OPTION       = VSTOI::RESPONSE_OPTION;
+    const STUDY                 = HASCO::STUDY;
+    const UNIT                  = SIO::UNIT;
 
   }
