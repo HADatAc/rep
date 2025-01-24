c71262abc909b18684c536800402adee39efdb9a
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-01-24 20:06:18 +0000

    ..

--- src/FusekiAPIConnector.php
+++ src/FusekiAPIConnector.php
@@ -1369,4 +1369,12 @@ class FusekiAPIConnector {
     return $totalValue;
   }
 
+  // Return List of Detector from Instrument to Fill on Process
+  public function detectorListFromInstrument($instrumentUri) {
+    $endpoint = "/hascoapi/api/instrument/detectors/".rawurlencode($instrumentUri);
+    $method = "GET";
+    $api_url = $this->getApiUrl();
+    $data = $this->getHeader();
+    return $this->perform_http_request($method,$api_url.$endpoint,$data);
+  }
 }
