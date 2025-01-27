95f7fb8252fdd934f31c92867fcbc171a1ba8db2
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-01-27 16:42:01 +0000

    tree

--- js/rep_tree.js
+++ js/rep_tree.js
@@ -144,11 +144,11 @@
             const selectedNode = data.node.original;
 
             if (selectedNode.id) {
-              console.log('typeNamespace detected:', selectedNode);
+              console.log(selectedNode);
               $selectNodeButton
                 .prop('disabled', false)
                 .removeClass('disabled')
-                .data('selected-value', selectedNode.uri ? selectedNode.uri : selectedNode.typeNamespace)
+                .data('selected-value', selectedNode.uri ? selectedNode.text + " [" + selectedNode.uri + "]" : selectedNode.typeNamespace)
                 .data('field-id', $('#tree-root').data('field-id')); // Associar o campo
             } else {
               //console.warn('No typeNamespace found. Disabling button.');
