9d98d54a40b532d7fcd907c1dd9a78f2a4921eb2
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-09 13:55:37 +0100

    .

--- src/ListKeywordTypePage.php
+++ src/ListKeywordTypePage.php
@@ -25,7 +25,8 @@ class ListKeywordTypePage {
     if ($keyword == NULL) {
       $keyword = "_";
     }
-    dpm("E=".$elementtype.", PR=".$project.", K=".$keyword.", T=".$type.", M=".$manageremail.", S=".$status.", P=".$page.", O=".$pagesize);
+    // dpm("E=".$elementtype.", PR=".$project.", K=".$keyword.", T=".$type.", M=".$manageremail.", S=".$status.", P=".$page.", O=".$pagesize);
+    \Drupal::logger('rep')->debug('E='.$elementtype.', PR='.$project.', K='.$keyword.', T='.$type.', M='.$manageremail.', S='.$status.', P='.$page.', O='.$pagesize);
 
     $api = \Drupal::service('rep.api_connector');
     $elements = $api->parseObjectResponse($api->listByKeywordType($elementtype,$pagesize,$offset,$project,$keyword,$type,$manageremail,$status),'listByKeywordType');
