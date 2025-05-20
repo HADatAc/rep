530733010762593adf54639af6ab010b1828d0af
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-20 13:34:23 +0100

    New VSTOI status

--- src/Vocabulary/VSTOI.php
+++ src/Vocabulary/VSTOI.php
@@ -72,6 +72,8 @@
     const DEPRECATED                      = VSTOI::VSTOI . "Deprecated";    // Cannot be deployed
     const DAMAGED                         = VSTOI::VSTOI . "Damaged";       // Cannot be deployed
     const DEPLOYED                        = VSTOI::VSTOI . "Deployed";      // Needs to become current before being deprecated or damaged
+    const SUSPENDED                       = VSTOI::VSTOI . "Suspended";
+    const CLOSED                          = VSTOI::VSTOI . "Closed";
 
     /*
      * PERMISSION
@@ -79,7 +81,7 @@
 
      const PUBLIC                          = VSTOI::VSTOI . "Public";
      const PRIVATE                         = VSTOI::VSTOI . "Private";
- 
+
      /*
      *    POSITIONS
      */
