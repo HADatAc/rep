11d5d71324209f5e0550ae978a99438571ef136c
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-29 17:56:28 +0100

    .

--- src/Entity/Stream.php
+++ src/Entity/Stream.php
@@ -152,185 +152,23 @@ class Stream {
     return $output;
   }
 
-  // public static function generateOutputStudy($list) {
-  //   $root_url = \Drupal::request()->getBaseUrl();
-  //   $useremail = \Drupal::currentUser()->getEmail();
-  //   $output   = [];
-
-  //   foreach ($list as $element) {
-  //     // 1) chave “segura”
-  //     $safe_key = base64_encode($element->uri);
-
-  //     // 2) link como render array
-  //     $link = t('<a target="_new" href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($element->uri).'">'.UTILS::namespaceUri($element->uri).'</a>');
-
-
-  //     // 3) resto dos campos
-  //     $datetime   = '';
-  //     if (isset($element->startedAt)) {
-  //       $dt = new \DateTime($element->startedAt);
-  //       $datetime = $dt->format('F j, Y \a\t g:i A');
-  //     }
-
-  //     $deployment = $element->deployment->label ?? '';
-  //     $sdd = t('<a target="_new" href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($element->semanticDataDictionary->uri).'">'.UTILS::namespaceUri($element->semanticDataDictionary->label).'</a>');
-  //     $source     = '';
-  //     if ($element->method === 'files') {
-  //       $source = t('Files');
-  //     }
-  //     elseif ($element->method === 'messages') {
-  //       $source = $element->messageProtocol
-  //         ? $element->messageProtocol . ' ' . t('messages')
-  //         : t('Messages');
-  //       if ($element->messageIP) {
-  //         $source .= ' @' . $element->messageIP;
-  //       }
-  //       if ($element->messagePort) {
-  //         $source .= ':' . $element->messagePort;
-  //       }
-  //     }
-
-  //     $previousUrl = base64_encode(Url::fromRoute('std.manage_study_elements', [
-  //         'studyuri' => base64_encode($element->uri),
-  //       ])->toString());
-
-  //     $view_da_str = base64_encode(Url::fromRoute('rep.describe_element', ['elementuri' => base64_encode($element->uri)])->toString());
-  //     $view_da_route = 'rep.describe_element';
-  //     $view_da = Url::fromRoute('rep.back_url', [
-  //       'previousurl' => $previousUrl,
-  //       'currenturl' => $view_da_str,
-  //       'currentroute' => 'rep.describe_element'
-  //     ]);
-
-  //     $edit_da_str = base64_encode(Url::fromRoute('rep.edit_mt', [
-  //       'elementtype' => 'da',
-  //       'elementuri' => base64_encode($element->uri),
-  //       'fixstd' => 'T',
-  //     ])->toString());
-  //     $edit_da = Url::fromRoute('rep.back_url', [
-  //       'previousurl' => $previousUrl,
-  //       'currenturl' => $edit_da_str,
-  //       'currentroute' => 'rep.edit_mt'
-  //     ]);
-
-  //     $delete_da = Url::fromRoute('rep.delete_element', [
-  //       'elementtype' => 'da',
-  //       'elementuri' => base64_encode($element->uri),
-  //       'currenturl' => $previousUrl,
-  //     ]);
-
-  //     $ingest_da = '';
-  //     $uningest_da = '';
-
-  //     // Criar os links adicionais
-  //     // Verificar se $view_da, $edit_da, $delete_da, $download_da são URLs válidas
-
-  //     $view_da = $view_da instanceof Url ? $view_da : Url::fromRoute('<nolink>');
-  //     $edit_da = $edit_da instanceof Url ? $edit_da : Url::fromRoute('<nolink>');
-  //     $delete_da = $delete_da instanceof Url ? $delete_da : Url::fromRoute('<nolink>');
-  //     $download_da = '/download-file/' . base64_encode($element->hasDataFile->filename) . '/' . base64_encode($element->isMemberOf->uri) . '/da';
-
-  //     $view_bto = Link::fromTextAndUrl(
-  //       Markup::create('<i class="fa-solid fa-eye"></i>'),
-  //       $view_da
-  //     )->toRenderable();
-  //     $view_bto['#attributes'] = [
-  //       'class' => ['btn', 'btn-sm', 'btn-secondary'],
-  //       'style' => 'margin-right: 10px;',
-  //     ];
-
-  //     if ($element->hasSIRManagerEmail === $useremail) {
-  //       $edit_bto = Link::fromTextAndUrl(
-  //         Markup::create('<i class="fa-solid fa-pen-to-square"></i>'),
-  //         $edit_da
-  //       )->toRenderable();
-  //       $edit_bto['#attributes'] = [
-  //         'class' => ['btn', 'btn-sm', 'btn-secondary'],
-  //         'style' => 'margin-right: 10px;',
-  //       ];
-  //     }
-
-  //     // Dete button
-  //     $data_url = $delete_da instanceof Url ? $delete_da->toString() : '#';
-
-  //     if ($element->hasSIRManagerEmail === $useremail) {
-  //       $delete_bto = [
-  //         '#markup' => Markup::create('<a href="#" class="btn btn-sm btn-secondary btn-danger delete-button"
-  //           data-url="' . $data_url . '"
-  //           onclick="return false;">
-  //           <i class="fa-solid fa-trash-can"></i>
-  //           </a>'),
-  //       ];
-
-  //       $ingest_bto = Link::fromTextAndUrl(
-  //         Markup::create('<i class="fa-solid fa-download"></i>'),
-  //         $view_da
-  //       )->toRenderable();
-  //       $ingest_bto['#attributes'] = [
-  //         'class' => ['btn', 'btn-sm', 'btn-secondary', !$element->hasDataFile->fileStatus == Constant::FILE_STATUS_UNPROCESSED ? 'disabled' : ''],
-  //         'style' => 'margin-right: 10px;',
-  //       ];
-
-  //       $uningest_bto = Link::fromTextAndUrl(
-  //         Markup::create('<i class="fa-solid fa-upload"></i>'),
-  //         $view_da
-  //       )->toRenderable();
-  //       $uningest_bto['#attributes'] = [
-  //         'class' => ['btn', 'btn-sm', 'btn-secondary', $element->hasDataFile->fileStatus == Constant::FILE_STATUS_UNPROCESSED ? 'disabled' : ''],
-  //         'style' => 'margin-right: 10px;',
-  //       ];
-  //     }
-
-  //     $download_bto = [
-  //       '#type' => 'link',
-  //       '#title' => Markup::create('<i class="fa-solid fa-save"></i>'),
-  //       '#url' => Url::fromUserInput("#", ['attributes' => ['data-download-url' => $download_da]]),
-  //       '#attributes' => [
-  //         'class' => ['btn', 'btn-sm', 'btn-secondary', 'download-url'],
-  //         'style' => 'margin-right: 10px;',
-  //       ],
-  //     ];
-
-  //     // Concatenar os links como HTML
-  //     $links = [
-  //       \Drupal::service('renderer')->render($view_bto),
-  //       \Drupal::service('renderer')->render($edit_bto),
-  //       \Drupal::service('renderer')->render($ingest_bto),
-  //       \Drupal::service('renderer')->render($uningest_bto),
-  //       \Drupal::service('renderer')->render($download_bto),
-  //       \Drupal::service('renderer')->render($delete_bto),
-  //     ];
-
-  //     $output[$safe_key] = [
-  //       'element_uri'        => $link,
-  //       'element_datetime'   => $datetime,
-  //       'element_deployment' => $deployment,
-  //       'element_sdd'        => $sdd,
-  //       'element_pattern'    => $element->datasetPattern ?? '-',
-  //       'element_source'     => $source,
-  //       'element_operations' => implode(' ', $links),
-  //     ];
-  //   }
-
-  //   return $output;
-  // }
   /**
- * Generate a renderable array of study elements, using plain HTML anchors
- * so no Url objects end up in attributes.
- *
- * @param object[] $list
- *   Array of element objects, each expected to have properties:
- *     - uri
- *     - startedAt
- *     - deployment->label
- *     - semanticDataDictionary->label & ->uri
- *     - datasetPattern
- *     - method, messageProtocol, messageIP, messagePort
- *     - hasSIRManagerEmail
- *
- * @return array
- *   Associative array of rows, keyed by base64-encoded URI.
- */
+   * Generate a renderable array of study elements, using plain HTML anchors
+   * so no Url objects end up in attributes.
+   *
+   * @param object[] $list
+   *   Array of element objects, each expected to have properties:
+   *     - uri
+   *     - startedAt
+   *     - deployment->label
+   *     - semanticDataDictionary->label & ->uri
+   *     - datasetPattern
+   *     - method, messageProtocol, messageIP, messagePort
+   *     - hasSIRManagerEmail
+   *
+   * @return array
+   *   Associative array of rows, keyed by base64-encoded URI.
+   */
   public static function generateOutputStudy(array $list) {
     $output   = [];
     // Site base URL, e.g. https://example.com
