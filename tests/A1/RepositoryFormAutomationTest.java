8aec77e19ab4defcb3ba371fc0680c34e5d18dee
kael <kaelloureiro@gmail.com>
2025-07-02 14:07:54 +0100

    logs para achar erros e botões debbuging

--- tests/A1/RepositoryFormAutomationTest.java
+++ tests/A1/RepositoryFormAutomationTest.java
@@ -52,6 +52,7 @@ public class RepositoryFormAutomationTest {
         // Espera explícita para página carregar o input
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("edit-name")));
 
+
         // Espera o botão login estar clicável
         wait.until(ExpectedConditions.elementToBeClickable(By.id("edit-submit")));
 
