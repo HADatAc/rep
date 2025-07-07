d715d5a686763414f339680f477e6d7782f69951
kael <kaelloureiro@gmail.com>
2025-07-07 15:29:37 +0100

    Debugging form auto

--- tests/A1/RegressionTEST.java
+++ tests/A1/RegressionTEST.java
@@ -14,10 +14,10 @@ public class RegressionTEST {
 
     @Test
     void runsetupanddeletetests() throws InterruptedException {
-        runTestClass(RepositoryFormAutomationTest.class);
+        runTestClass(AdminAuto.class);
         Thread.sleep(2000);
 
-        runTestClass(AdminAuto.class);
+        runTestClass(RepositoryFormAutomationTest.class);
         Thread.sleep(2000);
 
         runTestClass(FullUploadTestALL.class);
