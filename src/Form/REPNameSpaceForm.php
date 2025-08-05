b051d0138d9611383c69518101057e5cbbb45071
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-08-05 17:26:13 +0100

    fix delete all

--- src/Form/REPNameSpaceForm.php
+++ src/Form/REPNameSpaceForm.php
@@ -125,7 +125,7 @@ use Drupal\rep\Entity\Ontology;
         $form['actions_wrapper']['col_all_triples']['triples_all_actions']['delete_triples_submit'] = [
           '#type'       => 'submit',
           '#value'      => $this->t('Delete with URL'),
-          '#name'       => 'delete_all',
+          '#name'       => 'delete',
           '#attributes' => ['class' => ['btn', 'btn-danger', 'mb-2', 'delete-element-button']],
         ];
 
