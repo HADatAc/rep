405c7748631120a5567e882fe3e3fdf458d4c2ff
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-08-14 16:31:58 +0100

    .

--- src/Form/MapEntryPointsForm.php
+++ src/Form/MapEntryPointsForm.php
@@ -204,7 +204,7 @@ class MapEntryPointsForm extends FormBase {
 
     // 8) Attach libraries & pass settings to JS
     // $base = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getBaseUrl();
-    $base_url = Url::fromRoute('<front>', [], ['absolute' => TRUE, 'https' => TRUE])->toString() . \Drupal::request()->getBaseUrl();
+    $base_url = (\Drupal::request()->headers->get('x-forwarded-proto') === 'https' ? 'https://':'http://'). \Drupal::request()->getHost() . \Drupal::request()->getBaseUrl();
     // $form['#attached']['library'][] = 'rep/rep_tree';
     $form['#attached']['library'][] = 'rep/map_entry_points';
     $form['#attached']['drupalSettings']['repMap'] = [
