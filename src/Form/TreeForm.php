7ee8f13042516ec0aece1db1862e69235195d2a4
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-08-14 16:21:47 +0100

    .

--- src/Form/TreeForm.php
+++ src/Form/TreeForm.php
@@ -313,7 +313,7 @@ class TreeForm extends FormBase {
 
     // $base_url = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getBaseUrl();
     dpm(\Drupal::request()->isSecure(), 'Debug isSecure'); // Check if the request is secure
-    $base_url = (\Drupal::request()->isSecure() ? 'https://':'http://'). \Drupal::request()->getHost() . ':8081'. \Drupal::request()->getBaseUrl();
+    $base_url = (\Drupal::request()->isSecure() ? 'https://':'http://'). \Drupal::request()->getHost() . \Drupal::request()->getBaseUrl();
     dpm($base_url, 'Debug $base_url'); // See the base URL being used
     $form['#attached']['drupalSettings']['rep_tree'] = [
       'baseUrl' => $base_url,
