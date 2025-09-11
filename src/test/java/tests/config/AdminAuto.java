a38195f554238da647230ef6a9bb70f29f9cad14
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2025-09-11 17:26:47 +0100

    tests front ip

--- src/test/java/tests/config/AdminAuto.java
+++ src/test/java/tests/config/AdminAuto.java
@@ -1,18 +1,28 @@
 package tests.config;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.*;
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static tests.config.EnvConfig.*;
+import tests.base.BaseTest;
+import static tests.config.EnvConfig.FRONTEND_URL;
+import static tests.config.EnvConfig.LOGIN_URL;
+import static tests.config.EnvConfig.PASSWORD;
+import static tests.config.EnvConfig.USERNAME;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class AdminAuto {
+public class AdminAuto extends BaseTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
