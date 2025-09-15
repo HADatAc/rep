d539680d1d09ca0c2ab2f3d9f7ba9f081bc20b34
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2025-09-15 17:07:48 +0100

    alterações POM e ci.yml

--- src/test/java/tests/config/EnvConfig.java
+++ src/test/java/tests/config/EnvConfig.java
@@ -7,8 +7,8 @@ package tests.config;
 public class EnvConfig {
 
     // Base URLs
-    public static final String FRONTEND_URL = "18.203.69.17";
-    public static final String BACKEND_URL = "34.244.13.53";
+    public static final String FRONTEND_URL = "http://18.203.69.17";
+    public static final String BACKEND_URL = "http://34.244.13.53";
     //public static final String BACKEND_API_URL = BACKEND_URL + "/api";
     public static final String FUSEKI_URL = "http://localhost:3030";
     public static final String YASGUI_ENDPOINT = "http://localhost:8888";
