204e9a37139e8b4fa9bd289835211d3cd63c0afa
Kael <41486985+JyanseiKasch@users.noreply.github.com>
2025-09-15 18:06:48 +0100

    alterações POM e ci.yml

--- src/test/java/tests/A1/FullSetupWSandNHANES.java
+++ src/test/java/tests/A1/FullSetupWSandNHANES.java
@@ -45,6 +45,7 @@ public class FullSetupWSandNHANES {
         runTestClassAndAbortOnFailure(ConfigValidationTest.class);
         Thread.sleep(5000);
 */
+
         // All data upload
         runTestClassAndAbortOnFailure(FullUploadWS.class);
         Thread.sleep(5000);
