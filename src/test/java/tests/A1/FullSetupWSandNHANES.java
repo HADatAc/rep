df997f0cf18e742913cc0e3d5e579044440bc260
kael <kaelloureiro@gmail.com>
2025-09-23 17:06:03 +0100

    test ci reduzido

--- src/test/java/tests/A1/FullSetupWSandNHANES.java
+++ src/test/java/tests/A1/FullSetupWSandNHANES.java
@@ -81,6 +81,8 @@ public class FullSetupWSandNHANES {
             */
 
 
+
+
         } catch (Exception e) {
             System.err.println("⚠️ Test sequence aborted due to failure: " + e.getMessage());
         } finally {
